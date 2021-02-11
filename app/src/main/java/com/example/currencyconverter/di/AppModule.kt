19d49a874bbbd3aa0214dce78e49f11863ed51c2
Sergej Belko <Belko936@gmail.com>
2021-02-11 13:38:32 +0300

    -add models -interface to get data from internet -create Retrofit instance -setup hilt annotations

---
+++ app/src/main/java/com/example/currencyconverter/di/AppModule.kt
@@ -0,0 +1,26 @@
+package com.plcoding.currencyconverter.di
+
+import com.example.currencyconverter.CurrencyApplication
+import com.plcoding.currencyconverter.data.CurrencyApi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+private const val BASE_URL = "https://api.exchangeratesapi.io/"
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideCurrencyApi(): CurrencyApi = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+        .create(CurrencyApi::class.java)
+}
